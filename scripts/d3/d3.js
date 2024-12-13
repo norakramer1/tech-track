e55b492fd9f49dabab3f6734f1d14c289e2be212
nora kramer <74241562+norakramer1@users.noreply.github.com>
2024-12-13 12:11:15 +0100

    overlapping paintings + centered lines

--- scripts/d3/d3.js
+++ scripts/d3/d3.js
@@ -34,64 +34,61 @@ svg
 
 // Read the data
 export function renderD3(data) {
-  // Add X axis
-  var x = d3.scaleLinear().domain([1400, 2000]).range([0, width]);
-  // Move axis to bottom of graph
-  svg
+  const currentDate = new Date();
+  const xDomain = [1400, currentDate.getFullYear()];
+
+  // Add X axis with linear scale
+  let x = d3.scaleLinear().domain(xDomain).range([0, width]);
+  const xAxis = d3.axisBottom(x).ticks(10).tickFormat(d3.format("d")); // Year ticks with integer format
+
+  // Add axis to SVG
+  const xAxisG = svg
     .append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x));
+    .attr("transform", `translate(0,${height})`)
+    .call(xAxis);
 
   // Zoom functionality
-  let x2 = x;
-  const xAxis = d3.axisBottom(x2);
-  const xAxisG = svg.select("g").call(xAxis);
-
   const zoom = d3
     .zoom()
-    .scaleExtent([0.5, 80]) // This control how much you can unzoom and zoom
+    .scaleExtent([1, 80]) // Zoom range: 1x to 80x zoom
     .extent([
       [0, 0],
       [width, height],
     ])
     .on("zoom", function (event) {
-      // on zoom rescale x axis and update x position of all paintings and lines
-      x2 = event.transform.rescaleX(x);
-      xAxisG.call(d3.axisBottom(x2));
+      const newScale = event.transform.rescaleX(x);
 
-      d3.selectAll("#painting").attr("x", function (d) {
-        return x2(d.objectEndDate);
-      });
+      // Update the X axis dynamically
+      xAxis.scale(newScale);
+      xAxisG.call(xAxis);
 
+      // Update positions of paintings
+      d3.selectAll("#painting").attr("x", (d) => newScale(d.objectEndDate));
+
+      // Update positions of lines
       d3.selectAll(".painting-line")
-        .attr("stroke", "white")
-        .attr("x1", function (d) {
-          return x2(d.objectEndDate) + 100; // Adjust for the painting's center
-        })
-        .attr("x2", function (d) {
-          return x2(d.objectEndDate) + 100; // Match the x-coordinate
-        });
+        .attr("x1", (d) => newScale(d.objectEndDate) + 100)
+        .attr("x2", (d) => newScale(d.objectEndDate) + 100);
     });
 
+  // Add paintings to the graph
   // Add paintings to the graph
   svg
     .append("g")
     .attr("id", "paintings")
     .attr("clip-path", "url(#clipPath)")
-    .selectAll("dot")
+    .selectAll("image")
     .data(data)
     .enter()
     .append("image")
     .attr("id", "painting")
-    .attr("xlink:href", function (d) {
-      return d.primaryImageSmall;
-    })
-    .attr("x", function (d) {
-      return x(d.objectEndDate);
-    })
-    // .attr("y", height - 200) // Position paintings near the bottom of the canvas
-    //.attr("width", 200)
+    .attr("xlink:href", (d) => d.primaryImageSmall)
+    .attr("x", (d) => x(d.objectEndDate))
     .attr("height", 200)
+    .each(function () {
+      // Set initial width to prevent layout shift
+      d3.select(this).attr("width", 200);
+    })
     .on("click", (e, d) =>
       d3
         .select(".panel")
@@ -101,12 +98,12 @@ export function renderD3(data) {
           <img src="${d.primaryImageSmall}">
           <div>
             <h2>${d.title}</h2>
-            <h3> ${d.objectEndDate}</h3>
+            <h3>${d.objectEndDate}</h3>
             <h3><span>Made by:</span> ${d.artistDisplayName}</h3>
             <h3><span>About the artist:</span> ${d.artistDisplayBio}</h3>
             <h3><span>Credit:</span> ${d.creditLine}</h3>
           </div>
-          </li>`
+        </li>`
         )
         .transition()
         .duration(175)
@@ -122,16 +119,22 @@ export function renderD3(data) {
     .enter()
     .append("line")
     .attr("class", "painting-line")
-    .attr("x1", function (d) {
-      return x(d.objectEndDate) + 100; // Center line on painting
-    })
-    .attr("x2", function (d) {
-      return x(d.objectEndDate) + 100; // Match the x-coordinate
-    })
     .attr("y1", height - 140) // Start at the bottom of the painting
     .attr("y2", height) // End at the timeline
     .attr("stroke", "#9499b0")
-    .attr("stroke-width", 1);
+    .attr("stroke-width", 1)
+    .attr("x1", function (d, i) {
+      const painting = d3.selectAll("#painting").nodes()[i]; // Select corresponding painting
+      const paintingWidth = painting.getBBox().width; // Get width dynamically
+      const paintingX = x(d.objectEndDate); // Position of the painting
+      return paintingX + paintingWidth / 2; // Center of painting
+    })
+    .attr("x2", function (d, i) {
+      const painting = d3.selectAll("#painting").nodes()[i];
+      const paintingWidth = painting.getBBox().width;
+      const paintingX = x(d.objectEndDate);
+      return paintingX + paintingWidth / 2;
+    });
 
   // Call zoom function
   d3.select("svg").call(zoom);
--- scripts/storeData.js
+++ scripts/storeData.js
@@ -1,5 +1,5 @@
 export function storeData(data) {
   localStorage.setItem("paintings", JSON.stringify(data));
   let paintingsFromStorage = localStorage.getItem("paintings");
-  console.log(JSON.parse(paintingsFromStorage));
+  //console.log(JSON.parse(paintingsFromStorage));
 }
